1e7303f9c33d8fb693b85c2866f2d879646219b1
kraanzu <kraanzu@gmail.com>
2022-02-24 14:28:07 +0530

    Minor changes

--- setup.sh
+++ setup.sh
@@ -5,13 +5,10 @@ sudo pacman -Syyu
 sudo pacman --noconfirm -S yay
 yay --needed --noconfirm -S - < packages.txt
 
-chsh -s /usr/bin/fish
-
-# VIM Plugin manager
+# VIM Stuff
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-mkdir -p ~/.config/nvim/undodir # vim undos for life
+mkdir -p ~/.undoir # vim undo dir
 
 # STOW all the folders
 for f in *; do
