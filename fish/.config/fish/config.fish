c41400509a4200b2782b10d4fcec830aa5df4296
kraanzu <kraanzu@gmail.com>
2022-03-18 18:26:05 +0530

    Add more commands + utf8 fix

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,5 @@
 #--------CUSTOM FUNCTIONS---------------
- fish_vi_key_bindings
+fish_vi_key_bindings
 
 function com
     g++ $argv[1] && ./a.out
@@ -9,22 +9,22 @@ end
 function conf
     set val $argv[1]
 
-    if [ $val = "fish" ]
+    if [ $val = fish ]
         nvim ~/.config/fish/config.fish
 
-    else if [ $val = "starship" ]
+    else if [ $val = starship ]
         nvim ~/.config/starship.toml
 
-    else if [ $val = "qtile" ]
+    else if [ $val = qtile ]
         nvim ~/.config/qtile/config.py
 
-    else if [ $val = "alacritty" ]
+    else if [ $val = alacritty ]
         nvim ~/.config/alacritty/alacritty.yml
 
-    else if [ $val = "picom" ]
+    else if [ $val = picom ]
         nvim ~/.config/picom/picom.conf
 
-    else if [ $val = "vim" ]
+    else if [ $val = vim ]
         nvim ~/.config/nvim/init.vim
     else
         echo Not in the config list!!
@@ -33,11 +33,11 @@ end
 
 
 #----------------------------- BASICS -----------------------------------
-set fish_greeting                      # Supresses fish's intro message
-set TERM "xterm-256color"              # Sets the terminal type
+set fish_greeting # Supresses fish's intro message
+set TERM xterm-256color # Sets the terminal type
 
-set EDITOR "nvim"                      #Sets Editor to neovim
-set VISUAL "nvim"
+set EDITOR nvim #Sets Editor to neovim
+set VISUAL nvim
 
 set fish_color_normal '#88C0D0'
 set fish_color_autosuggestion '#B48EAD'
@@ -60,21 +60,27 @@ alias rm='rm -rfi'
 
 #----LS OUTPUT
 alias ls='exa -F --color=always --group-directories-first' # my preferred listing
-alias la='exa -a --color=always --group-directories-first'  # all files and dirs
-alias ll='exa -l --color=always --group-directories-first'  # long format
+alias la='exa -a --color=always --group-directories-first' # all files and dirs
+alias ll='exa -l --color=always --group-directories-first' # long format
 alias lt='exa -aT --color=always --group-directories-first' # tree listing
 
 # pacman and yay
 alias pacman='sudo pacman'
-alias pacsyu='sudo pacman -Syyu'                 # update only standard pkgs
-alias yaysua='yay -Sua --noconfirm'              # update only AUR pkgs
-alias yaysyu='yay -Syu --noconfirm'              # update standard pkgs and AUR pkgs
-alias unlock='sudo rm /var/lib/pacman/db.lck'    # remove pacman lock
-alias cleanup='sudo pacman -Rns (pacman -Qtdq)'  # remove orphaned packages
+alias pacsyu='sudo pacman -Syyu' # update only standard pkgs
+alias yaysua='yay -Sua --noconfirm' # update only AUR pkgs
+alias yaysyu='yay -Syu --noconfirm' # update standard pkgs and AUR pkgs
+alias unlock='sudo rm /var/lib/pacman/db.lck' # remove pacman lock
+alias cleanup='sudo pacman -Rns (pacman -Qtdq)' # remove orphaned packages
 
 # adding flags
-alias df='df -h'                          # human-readable sizes
-alias free='free -m'                      # show sizes in MB
+alias df='df -h' # human-readable sizes
+alias free='free -m' # show sizes in MB
+
+
+# Git Stuff
+alias uncommit="git reset HEAD~1"
+alias recommit="git commit --amend --no-edit"
+alias editcommit="git commit --amend"
 
 #-----MISC
 
@@ -90,4 +96,6 @@ alias grep='grep --color=auto'
 colorscript -e crunchbang-mini
 
 # export PYTHONDONTWRITEBYTECODE=1
+
+export LANG=en_IN.UTF-8
 starship init fish | source
