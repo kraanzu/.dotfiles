c2f26e2a658d9813e290b200f49890f4bfde873d
kraanzu <kraanzu@gmail.com>
2022-03-23 13:17:05 +0530

    Update battery script

--- qtile/.config/qtile/scripts/battery.sh
+++ qtile/.config/qtile/scripts/battery.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-while true
-do
-  ac=`acpi -b | grep -c "Charging"`
-  battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
-   if [ $battery_level -ge 99 ] && [ $ac -ge 1 ] ; then
-      notify-send "Battery Full" "Level: ${battery_level}%"
-      paplay ~/.config/qtile/sounds/discord.ogg
-    elif [ $battery_level -le 20 ] && [ $ac -le 0 ]; then
-      notify-send --urgency=CRITICAL "Battery Low" "Level: ${battery_level}%"
-      paplay ~/.config/qtile/sounds/discord.ogg
-   fi
- sleep 60
+while true; do
+  ac=$(acpi -b | grep -c "Charging")
+  battery_level=$(acpi -b | grep -P -o '[0-9]+(?=%)')
+  echo $battery_level
+  if [ -n $battery_level ]; then
+    echo No battery!
+    break
+  elif [ $battery_level -ge 99 ] && [ $ac -ge 1 ]; then
+    notify-send "Battery Full" "Level: ${battery_level}%"
+    paplay ~/.config/qtile/sounds/discord.ogg
+  elif [ $battery_level -le 20 ] && [ $ac -le 0 ]; then
+    notify-send --urgency=CRITICAL "Battery Low" "Level: ${battery_level}%"
+    paplay ~/.config/qtile/sounds/discord.ogg
+  fi
+  sleep 60
 done
+
+echo "Job finished"
