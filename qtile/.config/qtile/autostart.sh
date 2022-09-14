9880776f75a35ca66986e23838763a50242317a4
Murli Tawari <kraanzu@gmail.com>
2022-09-14 11:09:10 +0530

    Add caffeine

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -6,7 +6,7 @@ xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'& # Caps_Lock is annoying
 nm-applet &
 redshift -O 5000K &
 xfce4-clipman &
-# caffeine
+caffeine &
 bash ~/.config/qtile/scripts/monitor.sh &
 bash ~/.config/qtile/scripts/battery.sh &
 dunst -conf ~/.config/dunst/dunstrc &
