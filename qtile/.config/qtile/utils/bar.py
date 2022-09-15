f5cbb7ee0476ac83bed1835949fd3bff9d538d4f
Murli Tawari <kraanzu@gmail.com>
2022-09-15 10:32:54 +0530

    refactor: A lot easier config
    Move all widgets to different file
    TODO: Need to update this a bit more

--- qtile/.config/qtile/utils/bar.py
+++ qtile/.config/qtile/utils/bar.py
@@ -1,155 +1,23 @@
-from .colors import color
 import psutil
-
 from libqtile import bar
 
-# from qtile_extras import bar
-from qtile_extras.widget.decorations import RectDecoration
-from qtile_extras.widget import (
-    Spacer,
-    Sep,
-    Systray,
-    GroupBox,
-    Clock,
-    Volume,
-    Memory,
-    Battery,
-    TextBox,
-    Wlan,
-    NvidiaSensors,
-    Image,
-    StatusNotifier,
-)
-
 from utils.colors import color
-from utils.groups import group_dict
-
-
-SPACE = Spacer(length=8)
-
-group_box_settings = {
-    "borderwidth": 2,
-    "margin": 4,
-    "active": color["light3"],
-    "inactive": color["grey"],
-    "disable_drag": True,
-    "highlight_color": color["light2"],
-    "block_highlight_text_color": color["dark1"],
-    "highlight_method": "text",
-    "inactive_highlight_method": "block",
-    "this_current_screen_border": color["cyan"],
-    "foreground": color["light1"],
-    "urgent_border": color["red"],
-    "padding": 2.5,
-}
-
-
-def battery_status():
-    battery = psutil.sensors_battery()
-    if not battery:
-        return ""
-    if battery.power_plugged:
-        return ""
-    else:
-        if battery.percent > 20:
-            return ""
-        else:
-            return ""
-
-
-def get_decor(c: str):
-    return {
-        "foreground": color["dark2" if not "dark" in c else "light3"],
-        "decorations": [
-            RectDecoration(
-                colour=color[c],
-                radius=0,
-                filled=True,
-                padding_x=1,
-                padding_y=7,
-                line_width=1,
-            )
-        ],
-    }
-
-
-def IconWidget(icon: str, color: str) -> TextBox:
-    return TextBox(
-        font="SauceCodePro Nerd Font",
-        fontsize=23,
-        text=f" {icon} ",
-        **get_decor(color),
-    )
+from utils.default_widgets import *
 
 
 bottom_bar = bar.Bar(
     [
-        TextBox(
-            font="SauceCodePro Nerd Font",
-            foreground=color["blue"],
-            fontsize=25,
-            text=" ﲅ ",
-        ),
-        Sep(linewidth=2, size_percent=65, foreground=color["dark3"]),
-        SPACE,
-        GroupBox(
-            font="SauceCodePro Nerd Font",
-            fontsize=30,
-            visible_groups=[group_dict[i] for i in range(1, 10)],
-            **group_box_settings,
-        ),
+        *widget_favicon,
+        SEP,
+        widget_group_box,
         Spacer(),
-        Systray(
-            icon_size=18,
-            padding=6,
-            foreground=color["grey"],
-        ),
-        SPACE,
-        *(
-            [
-                Battery(
-                    format=" {char} {percent:2.0%} ",
-                    charge_char="",
-                    discharge_char="",
-                    fontsize=16,
-                    **get_decor("green"),
-                ),
-            ]
-            if psutil.sensors_battery()
-            else []
-        ),
-        SPACE,
-        IconWidget("蓼", "blue"),
-        Volume(
-            device="pulse",
-            fmt=" {} ",
-            **get_decor("dark3"),
-        ),
-        SPACE,
-        IconWidget("", "blue"),
-        Wlan(
-            format=" {essid}  ",
-            disconnected_message=" Disconnected ",
-            **get_decor("dark3"),
-        ),
-        # Memory(
-        #     fmt=" {} ",
-        #     format="{MemUsed: .0f} {mm}",
-        #     **get_decor("dark3"),
-        # ),
-        SPACE,
-        IconWidget("ﮌ", "blue"),
-        Clock(
-            format=" %H:%M ",
-            **get_decor("dark3"),
-        ),
-        SPACE,
-        IconWidget("", "blue"),
-        Clock(
-            format=" %A, %B %d ",
-            **get_decor("dark3"),
-        ),
-        SPACE,
+        *widget_systray,
+        *(widget_battery if psutil.sensors_battery() else []),
+        *widget_volume,
+        *widget_wifi,
+        *widget_clock,
+        *widget_day,
+        PAD,
     ],
     size=40,
     background=color["dark2"][0],
