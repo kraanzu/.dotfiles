dbc0fb56b848776c28d124823a01097caf5f603f
kraanzu <kraanzu@gmail.com>
2022-12-28 21:37:25 +0530

    feat: Fatter git gutter lines

--- nvim/.config/nvim/plugin/gitgutter.lua
+++ nvim/.config/nvim/plugin/gitgutter.lua
@@ -1,3 +1,3 @@
-vim.g.gitgutter_sign_added = '┃'
-vim.g.gitgutter_sign_modified = '┃'
-vim.g.gitgutter_sign_removed = '┃'
+vim.g.gitgutter_sign_added = '▌'
+vim.g.gitgutter_sign_modified = '▌'
+vim.g.gitgutter_sign_removed = '▌'
