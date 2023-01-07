3a8f008f2f15d2968ffd59957d80e2ed77907efd
kraanzu <kraanzu@gmail.com>
2023-01-07 09:58:08 +0530

    Add tmuxp

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -1,12 +1,10 @@
 #! /bin/bash
-feh --bg-fill ~/.config/qtile/wallpaper.png &
 lxsession &
+feh --bg-fill ~/.config/qtile/wallpaper.png &
 picom --config ~/.config/picom/picom.conf &
 xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'& # Caps_Lock is annoying
 redshift -O 5000K &
 xfce4-clipman &
 caffeine &
-bash ~/.config/qtile/scripts/monitor.sh &
-bash ~/.config/qtile/scripts/battery.sh &
 dunst -conf ~/.config/dunst/dunstrc &
-pactl load-module module-detect & # detect speakers
+find ~/.config/tmuxp/ -type f -exec tmuxp load -d "{}" \; > ~/x.txt # Create tmux sessions
