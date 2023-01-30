dfe6ddd8a76b7d80f7dd9bb38632ce7ed4028bee
kraanzu <kraanzu@gmail.com>
2023-01-30 15:57:28 +0530

    speedup: move to lazy.nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,12 +1,16 @@
-require "plugins"
-
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.cmd([[
+set termguicolors
+]])
 vim.g.vsnip_snippet_dir = "~/.config/vsnip"
+
+require("plugins")
 
 vim.api.nvim_set_keymap("n", "<leader>ff", ":Neoformat<cr>", { noremap = true })
 
 vim.cmd([[
-packadd nvim-treesitter
+set termguicolors
 colorscheme nord
 hi Normal guibg=NONE
 ]])
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,184 +1,139 @@
 local fn = vim.fn
 
--- Automatically install packer
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = fn.system({
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
 		"git",
 		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
 	})
-	print("Installing packer close and reopen Neovim...")
-	vim.cmd([[packadd packer.nvim]])
 end
+vim.opt.rtp:prepend(lazypath)
+return require("lazy").setup({
+	"nathom/filetype.nvim",
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
+	-- Debugguing
+	"nvim-lua/plenary.nvim",
+	"mfussenegger/nvim-dap",
 
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-	return
-end
+	-- MAKING LIFE EASIER
+	{ "preservim/tagbar", cmd = "TagbarToggle" },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = "BufRead",
+	},
+    {"nvim-telescope/telescope.nvim", cmd = "Telescope"},
+    {"sbdchd/neoformat", event = "BufRead"},
+	{ "jreybert/vimagit", cmd = "Magit" },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+        cmd = "Mason",
+	},
 
--- Have packer use a popup window
-packer.init({
-	display = {
-		open_fn = function()
-			return require("packer.util").float({ border = "rounded" })
+	-- AUTOCOMPLETE BABY
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			-- Automatically install LSPs to stdpath for neovim
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
+		config = function()
+			require("user.lsp")
+		end,
+		event = "BufRead",
+	},
+	{ "hrsh7th/cmp-nvim-lsp", event = "BufRead" },
+	{
+		"hrsh7th/nvim-cmp",
+		config = function()
+			require("user.cmp")
 		end,
 	},
-})
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/vim-vsnip",
+	{
+		"ray-x/lsp_signature.nvim",
+		config = function()
+			require("lsp_signature").setup({})
+		end,
+        event = "BufWrite",
+	},
 
-return require("packer").startup({
-	function()
-		-- Packer can manage itself
-		use("wbthomason/packer.nvim")
+	-- GIT STUFF
+	"airblade/vim-gitgutter",
+	{ "tpope/vim-fugitive", cmd = "G" },
 
-		-- Performance
-		-- use({
-		-- 	"lewis6991/impatient.nvim",
-		-- 	config = function()
-		-- 		require("impatient")
-		-- 	end,
-		-- })
-		use("nathom/filetype.nvim")
-		use("dstein64/vim-startuptime")
+	-- HELPERS
+    {"jiangmiao/auto-pairs"},
+	{ "tomtom/tcomment_vim", event = "BufRead" },
 
-		-- Debugguing
-		use("nvim-lua/plenary.nvim")
-		use("mfussenegger/nvim-dap")
+	-- EYE CANDY
+	{
+		"karb94/neoscroll.nvim",
+		config = function()
+			require("neoscroll").setup()
+		end,
+        event = "BufRead",
+	},
 
-		-- MAKING LIFE EASIER
-		use({ "preservim/tagbar", cmd = "TagbarToggle" })
-		use({
-			"nvim-treesitter/nvim-treesitter",
-			event = { "BufRead", "BufNewFile" },
-		})
-		use({ "nvim-telescope/telescope.nvim" })
-		use({ "sbdchd/neoformat" })
-		use({ "jreybert/vimagit", cmd = "Magit" })
-		use({
-			"williamboman/mason.nvim",
-			config = function()
-				require("mason").setup()
-			end,
-		})
+	"gbprod/nord.nvim",
+	{
+		"folke/todo-comments.nvim",
+		dependencies = "nvim-lua/plenary.nvim",
+		config = function()
+			require("todo-comments").setup({})
+		end,
+        event = "BufRead",
+	},
+	{ "machakann/vim-highlightedyank", event = "BufRead" },
+	{
+		"nvim-lualine/lualine.nvim",
+		config = function()
+			require("user.line")
+		end,
+		dependencies = { "kyazdani42/nvim-web-devicons" },
+	},
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup()
+		end,
+	},
 
-		-- AUTOCOMPLETE BABY
-		use({
-			"neovim/nvim-lspconfig",
-			config = function()
-				require("user.lsp")
-			end,
-			event = "BufEnter",
-		})
-		use({ "hrsh7th/cmp-nvim-lsp", event = "InsertEnter" })
-		use({
-			"hrsh7th/nvim-cmp",
-			after = "cmp-nvim-lsp",
-			config = function()
-				require("user.cmp")
-			end,
-		})
-		use({ "hrsh7th/cmp-buffer", after = "nvim-cmp" })
-		use({ "hrsh7th/vim-vsnip", after = "nvim-cmp" })
-		use({
-			"ray-x/lsp_signature.nvim",
-			config = function()
-				require("lsp_signature").setup({})
-			end,
-		})
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		config = function()
+			vim.opt.list = true
+			vim.opt.listchars:append("eol:↴")
+			require("indent_blankline").setup({
+				space_char_blankline = " ",
+				show_current_context = true,
+			})
+		end,
+	},
+	{
+		"onsails/lspkind.nvim",
+		config = function()
+			require("user.lspkind")
+		end,
+	},
 
-		-- GIT STUFF
-		use("airblade/vim-gitgutter")
-		use({ "tpope/vim-fugitive", cmd = "G" })
-
-		-- HELPERS
-		use("jiangmiao/auto-pairs")
-		use({ "tomtom/tcomment_vim", event = "BufRead" })
-
-		-- EYE CANDY
-		use({
-			"karb94/neoscroll.nvim",
-			config = function()
-				require("neoscroll").setup()
-			end,
-		})
-		-- use({ "arcticicestudio/nord-vim", event = "VimEnter" })
-
-		use("gbprod/nord.nvim")
-		use({
-			"folke/todo-comments.nvim",
-			requires = "nvim-lua/plenary.nvim",
-			config = function()
-				require("todo-comments").setup({})
-			end,
-		})
-		use({ "machakann/vim-highlightedyank", event = "BufRead" })
-		use({
-			"nvim-lualine/lualine.nvim",
-			config = function()
-				require("user.line")
-			end,
-			requires = { "kyazdani42/nvim-web-devicons" },
-		})
-		-- using packer.nvim
-		-- use({
-		-- 	"kdheepak/tabline.nvim",
-		-- 	config = function()
-		-- 		require("user.tabline")
-		-- 	end,
-		-- 	requires = { { "hoob3rt/lualine.nvim", opt = true }, { "kyazdani42/nvim-web-devicons"} },
-		-- })
-
-		use({
-			"norcalli/nvim-colorizer.lua",
-			config = function()
-				require("colorizer").setup()
-			end,
-		})
-
-		use({
-			"lukas-reineke/indent-blankline.nvim",
-			config = function()
-				vim.opt.list = true
-				vim.opt.listchars:append("eol:↴")
-				require("indent_blankline").setup({
-					space_char_blankline = " ",
-					show_current_context = true,
-				})
-			end,
-		})
-		use({
-			"onsails/lspkind.nvim",
-			config = function()
-				require("user.lspkind")
-			end,
-		})
-
-		-- LANGAUGE STUFF
-		use({ "dag/vim-fish", ft = { "fish" } })
-		use({ "cespare/vim-toml", ft = { "toml" } })
-		-- use({ "rust-lang/rust.vim", ft = { "rs" } })
-		-- use({
-		-- 	"simrat39/rust-tools.nvim",
-		-- 	ft = { "rs" },
-		-- 	config = function()
-		-- 		require("rust-tools").setup({})
-		-- 	end,
-		-- })
-
-		if packer_bootstrap then
-			require("packer").sync()
-		end
-	end,
+	-- LANGAUGE STUFF
+	{ "dag/vim-fish", ft = { "fish" } },
+	{ "cespare/vim-toml", ft = { "toml" } },
+	{ "rust-lang/rust.vim", ft = { "rs" } },
+	{
+		"simrat39/rust-tools.nvim",
+		ft = { "rs" },
+		config = function()
+			require("rust-tools").setup({})
+		end,
+	},
 })
