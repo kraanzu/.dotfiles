dddfb0313e10a594d90ba0d977fa5a0215a05ec1
kraanzu <kraanzu@gmail.com>
2023-02-16 08:36:00 +0530

    picon set `--experimental backends`

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 lxsession &
 feh --bg-fill ~/.config/qtile/wallpaper.png &
-picom --config ~/.config/picom/picom.conf &
+picom --experimental-backends --config ~/.config/picom/picom.conf &
 xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'& # Caps_Lock is annoying
 redshift -O 5000K &
 xfce4-clipman &
