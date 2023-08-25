7647dbc3c2fa62cf49bdc743a30c809ebc8bc3f9
kraanzu <kraanzu@gmail.com>
2023-08-26 01:08:49 +0530

    add marksman

--- nvim/.config/nvim/lua/user/lsp.lua
+++ nvim/.config/nvim/lua/user/lsp.lua
@@ -55,7 +55,7 @@ capabilities.textDocument.completion.completionItem.resolveSupport = {
 	},
 }
 
-local servers = { "pyright", "ccls"}
+local servers = { "pyright", "marksman"}
 for _, lsp in pairs(servers) do
 	require("lspconfig")[lsp].setup({
 		on_attach = on_attach,
