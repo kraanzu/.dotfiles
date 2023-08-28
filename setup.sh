aa2e0f8dc46d18122bb80d78083b72ebfb38fb8f
kraanzu <kraanzu@gmail.com>
2023-08-28 22:53:21 +0530

    helper comments

--- setup.sh
+++ setup.sh
@@ -5,12 +5,14 @@ sudo pacman -Syyu
 sudo pacman --noconfirm --needed -S yay
 
 # install all the installed packages
+# while loop so that if anything does not install
+# then the script doesn't stop
 while read pkg; do yay -S --needed --noconfirm $pkg; done < packages.txt
 
 mkdir -p ~/.undodir # nvim undo dir
 
-G='\033[0;32m'   #'0;32' is Green's ANSI color code
-Y='\033[1;32m'   #'1;32' is Yellow's ANSI color code
+G='\033[0;32m' 
+Y='\033[1;32m'
 R='\033[0;31m'
 B='\033[0;34m'
 END='\033[0m'
@@ -19,11 +21,12 @@ END='\033[0m'
 echo -e "${G}[+] Setting up configs...${END}"
 stow configs/
 
+# Installs nord icons
 echo -en "${G}[+] Setting up icons...  ${END}"
 mkdir -p ~/.icons
 tar -xf ~/.dotfiles/others/files/icons.tar.xz -C ~/.icons
 
+# Outro
 echo -e "${G}Done!${END}"
-
 echo
 echo -e "${R}Happy Hacking <3${END}"
