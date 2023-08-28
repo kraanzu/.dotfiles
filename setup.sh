18253c017c0debd0bcfb39f56962694f270e8993
kraanzu <kraanzu@gmail.com>
2023-08-28 22:50:25 +0530

    update installation script

--- setup.sh
+++ setup.sh
@@ -7,10 +7,7 @@ sudo pacman --noconfirm --needed -S yay
 # install all the installed packages
 while read pkg; do yay -S --needed --noconfirm $pkg; done < packages.txt
 
-mkdir -p ~/.undodir # vim undo dir
-
-# STOW all the configs
-echo "TIME TO STOW THE CONFIGSSS...\n\n"
+mkdir -p ~/.undodir # nvim undo dir
 
 G='\033[0;32m'   #'0;32' is Green's ANSI color code
 Y='\033[1;32m'   #'1;32' is Yellow's ANSI color code
@@ -18,23 +15,11 @@ R='\033[0;31m'
 B='\033[0;34m'
 END='\033[0m'
 
-for f in *; do
-  if [[ -d "$f" ]] && [[ "$f" != "others"  ]]; then
-    echo -e "${G}[+] Setting up ${f} ...${END}"
+# STOW all the configs
+echo -e "${G}[+] Setting up configs...${END}"
+stow configs/
 
-    # check for already-present configs
-    if [[ -d ~/.config/${f} ]]; then
-      echo -en "${B}[!] Config for ${f} already present... Moving to ~/.config/${f}.old${END}...  "
-      mv ~/.config/${f} ~/.config/${f}.old
-      echo -e "${G}Done!${END}"
-    fi
-
-    echo 
-    stow $f
-  fi
-done
-
-echo -en "${G}[+] Setting up icons..  ${END}"
+echo -en "${G}[+] Setting up icons...  ${END}"
 mkdir -p ~/.icons
 tar -xf ~/.dotfiles/others/files/icons.tar.xz -C ~/.icons
 
