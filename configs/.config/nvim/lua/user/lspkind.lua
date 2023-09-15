903cdafc791ecb22a6432cc30fd661929bb15b33
kraanzu <kraanzu@gmail.com>
2023-09-15 16:58:56 +0530

    cleanup an refactor

--- configs/.config/nvim/lua/user/lspkind.lua
+++ configs/.config/nvim/lua/user/lspkind.lua
@@ -1,24 +1,6 @@
 require("lspkind").init({
-	-- DEPRECATED (use mode instead): enables text annotations
-	--
-	-- default: true
-	-- with_text = true,
-
-	-- defines how annotations are shown
-	-- default: symbol
-	-- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
 	mode = "symbol_text",
-
-	-- default symbol map
-	-- can be either 'default' (requires nerd-fonts font) or
-	-- 'codicons' for codicon preset (requires vscode-codicons font)
-	--
-	-- default: 'default'
 	preset = "codicons",
-
-	-- override preset symbols
-	--
-	-- default: {}
 	symbol_map = {
 		Text = "",
 		Method = "",
