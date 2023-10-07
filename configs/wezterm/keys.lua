062c28fcecf67af0dd0cd322f467cbeb5d844431
kraanzu <kraanzu@gmail.com>
2023-10-07 20:54:23 +0530

    add `keys.lua`

---
+++ configs/wezterm/keys.lua
@@ -0,0 +1,15 @@
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+local M = {}
+
+function M.setup(config)
+	config.keys = {
+        -- handle vim's TComment
+		{ key = "/", mods = "CTRL", action = wezterm.action({ SendString = "\x1f" }) },
+		{ key = "RightArrow", mods = "CTRL", action = act.SwitchWorkspaceRelative(1) },
+		{ key = "LeftArrow", mods = "CTRL", action = act.SwitchWorkspaceRelative(-1) },
+	}
+end
+
+return M
