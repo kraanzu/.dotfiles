5796092f3726b66cb3da29653b8b1f1b0c9af763
kraanzu <kraanzu@gmail.com>
2023-10-07 21:46:27 +0530

    add `wezterm-mux-server`

--- configs/qtile/autostart.sh
+++ configs/qtile/autostart.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
 lxsession &
-# feh --bg-fill ~/.config/qtile/wallpaper.png &
 # xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'& # Caps_Lock is annoying
 redshift -O 5000K &
 xfce4-clipman &
 dunst -conf ~/.config/dunst/dunstrc &
 for i in ~/.config/qtile/startup/*; do $i; done
 nitrogen --restore &
-for i in ~/.config/tmuxp/*; do .local/bin/tmuxp load -d $i; done
 picom &
+wezterm-mux-server --daemonize &
