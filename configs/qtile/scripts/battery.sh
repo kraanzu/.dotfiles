f8c9225159c2dad21be64b693482cf04d4440d4d
kraanzu <kraanzu@gmail.com>
2023-10-14 14:25:02 +0530

    remove sounds

--- configs/qtile/scripts/battery.sh
+++ configs/qtile/scripts/battery.sh
@@ -8,12 +8,8 @@ while true; do
     break
   elif [ $battery_level -ge 99 ] && [ $ac -ge 1 ]; then
     notify-send "Battery Full" "Level: ${battery_level}%"
-    paplay ~/.config/qtile/sounds/discord.ogg
   elif [ $battery_level -le 20 ] && [ $ac -le 0 ]; then
     notify-send --urgency=CRITICAL "Battery Low" "Level: ${battery_level}%"
-    paplay ~/.config/qtile/sounds/discord.ogg
   fi
   sleep 60
 done
-
-echo "Job finished"
