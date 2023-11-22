88ade41b1fffb6a89d103d1ad92870367c5d1e21
kraanzu <kraanzu@gmail.com>
2023-11-22 22:35:53 +0530

    add caffeine

--- configs/qtile/autostart.sh
+++ configs/qtile/autostart.sh
@@ -2,6 +2,7 @@
 lxsession &
 # xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'& # Caps_Lock is annoying
 redshift -O 5000K &
+caffeine &
 xfce4-clipman &
 dunst -conf ~/.config/dunst/dunstrc &
 for i in ~/.config/startup/*; do $i; done
