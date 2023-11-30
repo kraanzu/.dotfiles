333d2da20d1ad90a3dfa695f17531528c459ec25
kraanzu <kraanzu@gmail.com>
2023-11-30 08:16:07 +0530

    remove caffiene (autostart lxsession)

--- configs/qtile/autostart.sh
+++ configs/qtile/autostart.sh
@@ -3,7 +3,6 @@ lxsession &
 # xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'& # Caps_Lock is annoying
 openrgb --profile purple &
 redshift -O 5000K &
-caffeine &
 xfce4-clipman &
 dunst -conf ~/.config/dunst/dunstrc &
 for i in ~/.config/startup/*; do $i; done
