75a0c8c3e32c0bc1fc4304476bbabea65c4ece29
kraanzu <kraanzu@gmail.com>
2023-12-20 22:56:07 +0530

    fix: oil load event

--- configs/nvim/lua/plugins/oil.lua
+++ configs/nvim/lua/plugins/oil.lua
@@ -1,7 +1,6 @@
 return {
 	"stevearc/oil.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	cmd = "Oil",
 	config = function()
 		require("oil").setup()
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
