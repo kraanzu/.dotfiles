30f01024c358a5603579101004d7af4e0eff24e0
kraanzu <kraanzu@gmail.com>
2023-12-20 12:41:12 +0530

    refactor init

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -1,9 +1,7 @@
-vim.cmd([[
-set termguicolors
-let g:gitgutter_terminal_reports_focus=0
-]])
+vim.cmd("set termguicolors")
 
 -- essentials
+vim.g.gitgutter_terminal_reports_focus = 0
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.vsnip_snippet_dir = "~/.config/vsnip"
@@ -12,8 +10,4 @@ vim.api.nvim_set_keymap("n", "<leader>ff", ":Neoformat<cr>", { noremap = true })
 -- setup plugins
 require("plugins")
 
--- extras
-vim.cmd([[
-colorscheme nord
-hi Normal guibg=NONE
-]])
+vim.cmd("colorscheme nord")
