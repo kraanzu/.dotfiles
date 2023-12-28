8580e00c778dfc45e536145b7302df2054365867
kraanzu <kraanzu@gmail.com>
2023-12-28 20:28:01 +0530

    add docstring plugin

--- configs/nvim/lua/plugins/docstring.lua
+++ configs/nvim/lua/plugins/docstring.lua
@@ -1,4 +1,8 @@
 return {
 	"heavenshell/vim-pydocstring",
 	build = "make install",
+	config = function()
+		vim.g.pydocstring_formatter = "google"
+        vim.api.nvim_set_keymap("n", "<leader>ds", ":Pydocstring<cr>", { noremap = true })
+	end,
 }
