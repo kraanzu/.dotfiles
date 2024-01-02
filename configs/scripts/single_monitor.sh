a7773f9bb2a5e5b1fc82c755bf1d257e1d82c18e
kraanzu <kraanzu@gmail.com>
2024-01-02 17:20:23 +0530

    remove unnecessary outputs

--- configs/scripts/single_monitor.sh
+++ configs/scripts/single_monitor.sh
@@ -2,5 +2,5 @@
 
 # Setup dual monitors
 if xrandr | grep -q 'HDMI-A-0 connected' ; then
-    xrandr --output DisplayPort-0 --off --output DisplayPort-1 --mode 1920x1080 --pos 1080x420 --rotate normal --output HDMI-A-0 --off --output HDMI-A-1 --off
+    xrandr --output DisplayPort-1 --mode 1920x1080 --pos 1080x420 --rotate normal --output HDMI-A-0 --off
 fi
