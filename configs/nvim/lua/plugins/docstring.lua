e93d1be52fedbf2a499c3d6fea5a5883d1332baf
kraanzu <kraanzu@gmail.com>
2024-01-15 20:32:21 +0530

    disable docstring for now

--- configs/nvim/lua/plugins/docstring.lua
+++ configs/nvim/lua/plugins/docstring.lua
@@ -1,8 +1,8 @@
 return {
-	"heavenshell/vim-pydocstring",
-	build = "make install",
-	config = function()
-		vim.g.pydocstring_formatter = "google"
-        vim.api.nvim_set_keymap("n", "<leader>ds", ":Pydocstring<cr>", { noremap = true })
-	end,
+	-- "heavenshell/vim-pydocstring",
+	-- build = "make install",
+	-- config = function()
+	-- 	vim.g.pydocstring_formatter = "google"
+    --     vim.api.nvim_set_keymap("n", "<leader>ds", ":Pydocstring<cr>", { noremap = true })
+	-- end,
 }
