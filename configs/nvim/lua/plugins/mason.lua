4c94784e91fa6656f91dadf13878ff997ca1f01d
kraanzu <kraanzu@gmail.com>
2024-02-01 22:11:09 +0530

    add mason-lspconfig

--- configs/nvim/lua/plugins/mason.lua
+++ configs/nvim/lua/plugins/mason.lua
@@ -6,4 +6,19 @@ return {
 		end,
 		cmd = "Mason",
 	},
+
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require("mason-lspconfig").setup({
+                automatic_installation = true,
+				ensure_installed = {
+                  "pyright",
+                  "lua_ls",
+                  "marksman",
+				},
+			})
+		end,
+	},
 }
