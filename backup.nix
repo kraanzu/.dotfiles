f7b10ea0fd43ada0d9ada47d886dc496518ba4b3
kraanzu <kraanzu@gmail.com>
2024-05-05 21:32:40 +0530

    try vimjoyer

---
+++ backup.nix
@@ -0,0 +1,20 @@
+{
+  description = "My Flake";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+  };
+  outputs = {self, nixpkgs, ... }:
+  let 
+    lib = nixpkgs.lib;
+  in
+  {
+    nixosConfigurations = {
+      kraanzu = lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+        ./configuration.nix 
+        ];
+      };
+    };
+  };
+}
--- flake.nix
+++ flake.nix
@@ -1,20 +1,22 @@
 {
-  description = "My Flake";
+  description = "Nixos config flake";
+
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = {self, nixpkgs, ... }:
-  let 
-    lib = nixpkgs.lib;
-  in
-  {
-    nixosConfigurations = {
-      kraanzu = lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-        ./configuration.nix 
-        ];
-      };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.kraanzu = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./configuration.nix
+        inputs.home-manager.nixosModules.default
+      ];
     };
   };
 }
