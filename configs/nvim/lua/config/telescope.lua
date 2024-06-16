e9ec192446a974f06c2eee168e67bd6f91f35b93
kraanzu <kraanzu@gmail.com>
2024-06-16 21:19:50 +0530

    add telescope config

---
+++ configs/nvim/lua/config/telescope.lua
@@ -0,0 +1,10 @@
+local actions = require("telescope.actions")
+require("telescope").setup{
+  defaults = {
+    mappings = {
+      i = {
+        ["<esc>"] = actions.close
+      },
+    },
+  }
+}
--- configs/nvim/lua/plugins/telescope.lua
+++ configs/nvim/lua/plugins/telescope.lua
@@ -3,6 +3,7 @@ return {
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.2",
 		config = function()
+			require("config.telescope")
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>tp", builtin.git_files, {})
 			vim.keymap.set("n", "<leader>tr", builtin.live_grep, {})
