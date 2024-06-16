d69a18f757dfd7016314e2961750a671857899a7
kraanzu <kraanzu@gmail.com>
2024-06-16 21:47:17 +0530

    fullscreen telescope

--- configs/nvim/lua/config/telescope.lua
+++ configs/nvim/lua/config/telescope.lua
@@ -1,19 +1,25 @@
 local actions = require("telescope.actions")
 local builtin = require("telescope.builtin")
 
--- Keymaps 
+-- Keymaps
 vim.keymap.set("n", "<leader>tp", builtin.git_files, {})
 vim.keymap.set("n", "<leader>tr", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>tb", builtin.buffers, {})
 
-
 -- Setup
-require("telescope").setup{
-  defaults = {
-    mappings = {
-      i = {
-        ["<esc>"] = actions.close
-      },
-    },
-  }
-}
+require("telescope").setup({
+	defaults = {
+		layout_strategy = "vertical", -- Use vertical layout strategy
+		layout_config = {
+			prompt_position = "bottom", -- Position prompt at the top
+			width = 0.99, -- Full width of the screen
+			height = 0.99, -- Full height of the screen
+			preview_cutoff = 15, -- Adjust preview cutoff if needed
+		},
+		mappings = {
+			i = {
+				["<esc>"] = actions.close,
+			},
+		},
+	},
+})
