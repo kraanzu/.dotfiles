554be18f6adde06d83b8ec626cf75c151a799fda
kraanzu <kraanzu@gmail.com>
2024-06-17 21:15:21 +0530

    mv util funcs into sep file

--- configs/qtile/bars/base.py
+++ configs/qtile/bars/base.py
@@ -1,7 +1,6 @@
-import psutil
-import subprocess
 from qtile_extras.widget import Spacer, Sep
-from utils.colors import color
+from utils import color 
+from utils.functions import *
 from qtile_extras.widget import (
     TextBox,
     GroupBox,
@@ -31,45 +30,6 @@ DARK = "dark3"
 
 
 # -------------- BASIC UTIL FUNCTIONS ---------------------
-
-
-def get_default_sink() -> str:
-    sink = subprocess.check_output(["pactl", "get-default-sink"], text=True).strip("\n")
-    return sink
-
-
-def get_running_sink_form_factor():
-    sink = get_default_sink()
-    pactl_output = subprocess.check_output(["pactl", "list", "sinks"], text=True)
-    lines = pactl_output.split("\n")
-    in_running_sink = False
-    form_factor = None
-    for line in lines:
-        if sink in line:
-            in_running_sink = True
-        elif in_running_sink and "device.form_factor" in line:
-            form_factor = line.split("=")[1].strip()
-            break
-    return form_factor
-
-
-def is_headphone_connected() -> bool:
-    headphone_string = ["headphone", "headset"]
-    form_factor = get_running_sink_form_factor() or ""
-    return any(i in form_factor for i in headphone_string)
-
-
-def battery_status():
-    battery = psutil.sensors_battery()
-    if not battery:
-        return ""
-    if battery.power_plugged:
-        return ""
-    else:
-        if battery.percent > 20:
-            return ""
-        else:
-            return ""
 
 
 # ---------------- DEFAULTS -----------------
