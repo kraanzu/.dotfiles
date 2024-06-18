0d9ed102405be93a610b9594b738893d247be9aa
kraanzu <kraanzu@gmail.com>
2024-06-18 21:47:07 +0530

    set horizontal layout

--- configs/nvim/lua/config/telescope.lua
+++ configs/nvim/lua/config/telescope.lua
@@ -9,12 +9,13 @@ vim.keymap.set("n", "<leader>tb", builtin.buffers, {})
 -- Setup
 require("telescope").setup({
 	defaults = {
-		layout_strategy = "vertical", -- Use vertical layout strategy
+		layout_strategy = "horizontal", -- Use vertical layout strategy
 		layout_config = {
 			prompt_position = "bottom", -- Position prompt at the top
 			width = 0.99, -- Full width of the screen
 			height = 0.99, -- Full height of the screen
-			preview_cutoff = 15, -- Adjust preview cutoff if needed
+            -- set preview width to 0.65 of the screen
+            preview_width = 0.5,
 		},
 		mappings = {
 			i = {
