e75ca2bfa3f29650162486cfc0f88dc8ee5a65ad
kraanzu <kraanzu@gmail.com>
2024-07-14 18:54:19 +0530

    update telescope ui

--- configs/nvim/lua/config/telescope.lua
+++ configs/nvim/lua/config/telescope.lua
@@ -1,4 +1,3 @@
-local actions = require("telescope.actions")
 local builtin = require("telescope.builtin")
 
 -- Keymaps
@@ -9,18 +8,50 @@ vim.keymap.set("n", "<leader>tb", builtin.buffers, {})
 -- Setup
 require("telescope").setup({
 	defaults = {
-		layout_strategy = "horizontal", -- Use vertical layout strategy
-		layout_config = {
-			prompt_position = "bottom", -- Position prompt at the top
-			width = 0.99, -- Full width of the screen
-			height = 0.99, -- Full height of the screen
-            -- set preview width to 0.65 of the screen
-            preview_width = 0.5,
+		vimgrep_arguments = {
+			"rg",
+			"-L",
+			"--color=auto",
+			"--no-heading",
+			"--with-filename",
+			"--line-number",
+			"--column",
+			"--smart-case",
 		},
-		mappings = {
-			i = {
-				["<esc>"] = actions.close,
+		prompt_prefix = "   ",
+		selection_caret = "  ",
+		entry_prefix = "  ",
+		initial_mode = "insert",
+		selection_strategy = "reset",
+		sorting_strategy = "ascending",
+		layout_strategy = "horizontal",
+		layout_config = {
+			horizontal = {
+				prompt_position = "top",
+				preview_width = 0.55,
+				results_width = 0.8,
 			},
+			vertical = {
+				mirror = false,
+			},
+			width = 0.99,
+			height = 0.99,
+			preview_cutoff = 120,
+		},
+		file_sorter = require("telescope.sorters").get_fuzzy_file,
+		generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+		path_display = { "truncate" },
+		winblend = 0,
+		border = {},
+        borderchars = { "━", "┃", "━", "┃", "┏", "┓", "┛", "┗" },
+		color_devicons = true,
+		set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+		file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+		grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+		qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+		buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+		mappings = {
+			n = { ["q"] = require("telescope.actions").close },
 		},
 	},
 })
