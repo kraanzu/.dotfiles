ac72435e26e1a45d6ce05f633c9ee272b57b3b0f
kraanzu <kraanzu@gmail.com>
2024-08-27 10:19:23 +0530

    fix: nerdfont icons

--- configs/nvim/lua/config/lsp.lua
+++ configs/nvim/lua/config/lsp.lua
@@ -35,7 +35,7 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagn
 	severity_sort = true,
 })
 
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = "󰅙 ", Warn = " ", Hint = "󰌵 ", Info = " " }
 for type, icon in pairs(signs) do
 	local hl = "DiagnosticSign" .. type
 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
