973db63b9845e403a612409671e117c3f8a28200
kraanzu <kraanzu@gmail.com>
2024-08-28 21:08:24 +0530

    update urls for nixkgs and home-manager

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Kraanzu's nix config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
