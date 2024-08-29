f4a4bac3cef1a854435d7a305d16192595246017
kraanzu <kraanzu@gmail.com>
2024-08-29 17:57:17 +0530

    auto trigger nix-shell for shell.nix

--- configs/fish/triggers.fish
+++ configs/fish/triggers.fish
@@ -1,8 +1,18 @@
 function react_to_pwd --on-variable PWD
-    if test -d "$PWD/.venv"
-        source "$PWD/.venv/bin/activate.fish"
+    # Check if we are already inside a nix-shell
+    if test "$IN_NIX_SHELL" = "impure"
+        return
     end
-    starship init fish | source
+
+    # Check if `shell.nix` exists in the current directory
+    if not test -e "$PWD/shell.nix"
+        return
+    end
+
+    # Start `nix-shell` and run Fish
+    nix-shell --run "fish"
 end
 
-react_to_pwd # auto trigger on startup
+# Call the function to handle the current directory
+react_to_pwd
+
