07b33a9316a5d3ffa4e55b357fa87c525824a7b2
kraanzu <kraanzu@gmail.com>
2024-08-30 11:37:54 +0530

    enable bluetooth

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -94,6 +94,8 @@ in
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
