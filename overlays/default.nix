4087b8d1dee9356a6845ae47b4e8cc8a79cd8cc2
kraanzu <kraanzu@gmail.com>
2024-08-30 17:00:12 +0530

    fix: remove pkgs

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,11 +1,5 @@
 # This file defines overlays
 {inputs, ...}: {
-  # This one brings our custom packages from the 'pkgs' directory
-  additions = final: _prev: import ../pkgs final.pkgs;
-
-  # This one contains whatever you want to overlay
-  # You can change versions, add patches, set compilation flags, anything really.
-  # https://nixos.wiki/wiki/Overlays
   modifications = final: prev: {
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
