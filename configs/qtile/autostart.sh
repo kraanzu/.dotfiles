cd8df7ec55f4c4defc1710225dba034132e79d23
kraanzu <kraanzu@gmail.com>
2024-08-30 20:23:53 +0530

    add bash for bash startup files

--- configs/qtile/autostart.sh
+++ configs/qtile/autostart.sh
@@ -1,11 +1,8 @@
-#! /bin/bash
 lxsession &
-# xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'& # Caps_Lock is annoying
 openrgb --profile purple &
 xfce4-clipman &
 dunst -conf ~/.config/dunst/dunstrc &
-for i in ~/.config/startup/*; do $i; done
-# nitrogen --restore &
+for i in ~/.config/startup/*; do bash $i; done
 picom &
 wezterm-mux-server --daemonize &
 firefox&
