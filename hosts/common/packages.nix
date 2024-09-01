de0b881534df22ce521d7fb6dd242e07370c2435
kraanzu <kraanzu@gmail.com>
2024-09-01 13:36:33 +0530

    separate pkgs

--- hosts/common/packages.nix
+++ hosts/common/packages.nix
@@ -1,15 +1,10 @@
 {pkgs, pkgs-unstable, ...}: {
   environment.systemPackages = (with pkgs; [
     # basic utilties
-    networkmanagerapplet
-    blueberry
-    xfce.xfce4-clipman-plugin
-    caffeine-ng
     killall
     wget
     git
     htop
-    lxde.lxsession
     os-prober
     geoclue2
     unzip
@@ -25,31 +20,6 @@
     starship
     eza
     python311Packages.ipython
-
-    # terminal
-    wezterm
-    kitty
-
-    # gui utils
-    sddm
-    xfce.thunar
-    flameshot
-    dunst
-    rofi
-    vlc
-    gnome.gnome-disk-utility
-    gnome.nautilus
-
-    # extras
-    alejandra
-    discord
-    qtile
-    nodejs_22
-    poetry
-    python311Packages.pip
-
-    # languages
-    zig
   ])
 
   ++
---
+++ hosts/kraanzu/packages/default.nix
@@ -0,0 +1,9 @@
+{
+  pkgs,
+  inputs,
+  ...
+}: {
+  imports = [
+    ./utilities.nix
+  ];
+}
