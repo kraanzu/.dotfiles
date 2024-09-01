51ef51a80bec16194757c983b562a310a6c8dde4
kraanzu <kraanzu@gmail.com>
2024-09-01 17:57:29 +0530

    remove comments

--- modules/nixos/sddm_chili/sddm_chili.nix
+++ modules/nixos/sddm_chili/sddm_chili.nix
@@ -1,13 +1,13 @@
 {pkgs}:
 pkgs.stdenv.mkDerivation rec {
   pname = "sddm-chili";
-  version = "0.1.5"; # Adjust the version as needed
+  version = "0.1.5";
 
   src = pkgs.fetchFromGitHub {
     owner = "MarianArlt";
     repo = pname;
     rev = version;
-    sha256 = "sha256-wxWsdRGC59YzDcSopDRzxg8TfjjmA3LHrdWjepTuzgw="; # Replace with the correct hash
+    sha256 = "sha256-wxWsdRGC59YzDcSopDRzxg8TfjjmA3LHrdWjepTuzgw=";
   };
 
   installPhase = ''
