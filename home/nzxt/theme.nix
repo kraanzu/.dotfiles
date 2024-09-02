5d7ebfac1444ae265a807f5dff56a9f840c08ff2
kraanzu <kraanzu@gmail.com>
2024-09-02 22:11:12 +0530

    update gtk theme

--- home/nzxt/theme.nix
+++ home/nzxt/theme.nix
@@ -2,12 +2,12 @@
   gtk.enable = true;
   qt.enable = true;
 
-  gtk.theme.name = "Nordic";
+  gtk.theme.name = "Nordic-bluish-accent";
   gtk.theme.package = pkgs.nordic;
 
   gtk.iconTheme.name = "Nordic-darker";
   gtk.iconTheme.package = pkgs.nordic;
 
-  gtk.cursorTheme.name = "Nordic-cursor";
+  gtk.cursorTheme.name = "Nordic-cursors";
   gtk.cursorTheme.package = pkgs.nordic;
 }
