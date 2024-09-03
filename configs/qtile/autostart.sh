8c856cacaac880e1d289d32720f5cb2fe9dee0f0
kraanzu <kraanzu@gmail.com>
2024-09-03 13:14:26 +0530

    remove picom from autostart

--- configs/qtile/autostart.sh
+++ configs/qtile/autostart.sh
@@ -3,7 +3,6 @@ openrgb --profile purple &
 xfce4-clipman &
 dunst -conf ~/.config/dunst/dunstrc &
 for i in ~/.config/startup/*; do bash $i; done
-picom &
 wezterm-mux-server --daemonize &
 firefox&
 discord&
