4b42456dd6d427b800d0174fe7d39ed53b253a1a
kraanzu <kraanzu@gmail.com>
2024-09-05 09:51:44 +0530

    remove dusnt from autostart

--- configs/qtile/autostart.sh
+++ configs/qtile/autostart.sh
@@ -1,7 +1,6 @@
 lxsession &
 openrgb --profile purple &
 xfce4-clipman &
-dunst -conf ~/.config/dunst/dunstrc &
 for i in ~/.config/startup/*; do bash $i; done
 wezterm-mux-server --daemonize &
 firefox&
