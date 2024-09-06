d9ce1695dcbe6da8e0eaa536704bc0c6b8491a90
kraanzu <kraanzu@gmail.com>
2024-09-06 10:02:14 +0530

    refactor: mode list

--- modules/home-manager/rofi/rofi.nix
+++ modules/home-manager/rofi/rofi.nix
@@ -5,10 +5,20 @@
   osConfig,
   ...
 }: let
-  default_modes = "window,drun,run,ssh,combi";
-  plugin_modes = "emoji";
-  power_mode = "power-menu:${pkgs.rofi-power-menu}/bin/rofi-power-menu";
-  rofi_modes = "${default_modes},${plugin_modes},${power_mode}";
+  rofi_modes = [
+    # default modes
+    "window"
+    "drun"
+    "run"
+    "ssh"
+    "combi"
+
+    # simple plugins
+    "emoji"
+
+    # extra config
+    "power-menu:${pkgs.rofi-power-menu}/bin/rofi-power-menu"
+  ];
 in {
   config = lib.mkIf osConfig.mynix.x11.enable {
     programs.rofi = {
@@ -18,7 +28,7 @@ in {
         rofi-emoji
       ];
       extraConfig = {
-        modes = rofi_modes;
+        modes = lib.concatStringsSep "," rofi_modes;
       };
 
       theme = import ./rofi-theme.nix {
