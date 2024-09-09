3695fb34cdb8098060df35d5353fde5ef743cb3b
kraanzu <kraanzu@gmail.com>
2024-09-09 21:47:50 +0530

    add cleanup command for fish

--- hosts/common/fish.nix
+++ hosts/common/fish.nix
@@ -3,6 +3,6 @@
 
   programs.fish.shellAliases = {
     nix-shell = "nix-shell --run fish";
+    cleanup = "nix-env --delete-generations 3d &&  home-manager expire-generations \"-3 days\" && nix-collect-garbage";
   };
-
 }
