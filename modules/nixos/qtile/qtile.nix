3890ddacaddaed0d437375f52867a4c2115adc97
kraanzu <kraanzu@gmail.com>
2024-09-11 20:13:41 +0530

    add dbus-next

--- modules/nixos/qtile/qtile.nix
+++ modules/nixos/qtile/qtile.nix
@@ -2,8 +2,7 @@
   config,
   lib,
   ...
-}:
-with lib; {
+}: {
   options = {
     mynix.wm.qtile.enable = lib.mkOption {
       type = lib.types.bool;
@@ -12,13 +11,17 @@ with lib; {
     };
   };
 
-  config = mkIf config.mynix.wm.qtile.enable {
+  config = lib.mkIf config.mynix.wm.qtile.enable {
     mynix.x11.enable = true;
 
     services.xserver.windowManager.qtile = {
       enable = true;
       backend = "x11";
-      extraPackages = p: with p; [qtile-extras];
+      extraPackages = p:
+        with p; [
+          qtile-extras
+          dbus-next
+        ];
     };
   };
 }
