ab5087f77ada418162def2d013df5c624fc1cc8e
kraanzu <kraanzu@gmail.com>
2024-09-26 10:26:25 +0530

    auto trigger copliot

--- configs/nvim/lua/plugins/copilot.lua
+++ configs/nvim/lua/plugins/copilot.lua
@@ -1,8 +1,12 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
-  config = function()
-    require("copilot").setup({})
-  end,
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	config = function()
+		require("copilot").setup({
+			suggestion = {
+				auto_trigger = true,
+			},
+		})
+	end,
 }
