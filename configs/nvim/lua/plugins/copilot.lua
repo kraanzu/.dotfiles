6a5a8687fc795725dce7679637d3e84947dc26f1
kraanzu <kraanzu@gmail.com>
2024-09-26 10:45:12 +0530

    add keymaps for copliot

--- configs/nvim/lua/plugins/copilot.lua
+++ configs/nvim/lua/plugins/copilot.lua
@@ -5,7 +5,18 @@ return {
 	config = function()
 		require("copilot").setup({
 			suggestion = {
+				enabled = true,
 				auto_trigger = true,
+				hide_during_completion = false,
+				debounce = 75,
+				keymap = {
+					accept = "<M-l>",
+					accept_word = false,
+					accept_line = false,
+					next = "<M-]>",
+					prev = "<M-[>",
+					dismiss = "<C-]>",
+				},
 			},
 		})
 	end,
