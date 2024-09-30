774683c151e05d49f1f2e3c0e7a178ac0982d70a
kraanzu <kraanzu@gmail.com>
2024-09-30 19:07:37 +0530

    refactor: remove double colors

--- configs/qtile/utils/colors.py
+++ configs/qtile/utils/colors.py
@@ -1,25 +1,25 @@
 class Color:
     extra_dark = ["#252a34", "#252a34"]
 
-    dark1 = ["#2e3440", "#2e3440"]
-    dark2 = ["#3b4252", "#3b4252"]
-    dark3 = ["#434c5e", "#434c5e"]
-    dark4 = ["#4c566a", "#4c566a"]
+    dark1 = "#2e3440"
+    dark2 = "#3b4252"
+    dark3 = "#434c5e"
+    dark4 = "#4c566a"
 
-    light1 = ["#d8dee9", "#d8dee9"]
-    light2 = ["#e5e9f0", "#e5e9f0"]
-    light3 = ["#e5e9f0", "#e5e9f0"]
+    light1 = "#d8dee9"
+    light2 = "#e5e9f0"
+    light3 = "#e5e9f0"
 
-    red = ["#bf616a", "#bf616a"]
-    green = ["#a3be8c", "#a3be8c"]
-    yellow = ["#ebcb8b", "#ebcb8b"]
-    magenta = ["#b48ead", "#b48ead"]
-    grey = ["#4c566a", "#4c566a"]
-    orange = ["#d08770", "#d08770"]
-    cyan = ["#88c0d0", "#88c0d0"]
-    cyan2 = ["#8fbcbb", "#8fbcbb"]
-    blue = ["#84A7C0", "#84A7C0"]
-    blue2 = ["#5e81ac", "#5e81ac"]
+    red = "#bf616a"
+    green = "#a3be8c"
+    yellow = "#ebcb8b"
+    magenta = "#b48ead"
+    grey = "#4c566a"
+    orange = "#d08770"
+    cyan = "#88c0d0"
+    cyan2 = "#8fbcbb"
+    blue = "#84A7C0"
+    blue2 = "#5e81ac"
 
     def __getitem__(self, item):
         return getattr(self, item)
