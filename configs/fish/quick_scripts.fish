ca089252dcd1cdcf70ba80e945deb89d2d9a0815
kraanzu <kraanzu@gmail.com>
2024-10-04 09:12:57 +0530

    Revert "try fix: remove fish"
    This reverts commit 59c06dccbca2910eda7ddbc31a02f805a719a34c.

---
+++ configs/fish/quick_scripts.fish
@@ -0,0 +1,21 @@
+# compile and run c/c++
+function com
+    g++ $argv[1] && ./a.out
+end
+
+# git diff for last commit
+function gdiff
+    set COMMIT $argv[1]
+    git diff $COMMIT~ $COMMIT
+end
+
+# push to android
+function apush
+    adb push $argv[1] /sdcard/AAA
+end
+
+# MISC
+alias dclean='docker system prune -a --volumes'
+alias kayo='gdrive download --recursive --skip --path ~/HDD/anime/'
+alias loff='xrandr --output eDP1 --off'
+alias lonn='xrandr --output eDP1 --auto'
---
+++ configs/fish/utils.fish
@@ -0,0 +1,35 @@
+function ext
+    set destination $argv[-1]
+
+    for file in $argv[1..-2]
+        if string match -q '*.tar' $file
+            tar -xf $file -C $destination
+        else if string match -q '*.tar.gz' $file
+            tar -xzf $file -C $destination
+        else if string match -q '*.tgz' $file
+            tar -xzf $file -C $destination
+        else if string match -q '*.zip' $file
+            unzip $file -d $destination
+        else if string match -q '*.rar' $file
+            unrar x $file $destination
+        else
+            echo "Unsupported archive type for file: $file"
+        end
+    end
+end
+
+# merge develop branch and push
+function mmm
+    git checkout main
+    git merge develop
+    git push
+    git checkout develop
+end
+
+function shell
+  if test ! -d .venv
+    python3 -m venv .venv
+  end
+
+  source .venv/bin/activate.fish
+end
