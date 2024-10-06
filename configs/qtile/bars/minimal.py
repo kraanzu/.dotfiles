4c524acc855e617ea8db7390f4efc17ee2daf587
kraanzu <kraanzu@gmail.com>
2024-10-06 23:04:14 +0530

    better bar

--- configs/qtile/bars/minimal.py
+++ configs/qtile/bars/minimal.py
@@ -1,5 +1,6 @@
 from libqtile import bar
 from qtile_extras.widget.decorations import RectDecoration, BorderDecoration
+from qtile_extras.widget.groupbox2 import Box, GroupBoxRule
 from utils.colors import color
 from bars.base import *
 from bars.base import Widget
@@ -12,7 +13,6 @@ COMMON_ATTRS = {
             radius=1,
             filled=True,
             padding_y=6.5,
-            padding_x=2,
             group=True,
             border_width=5,
         ),
@@ -20,7 +20,7 @@ COMMON_ATTRS = {
             border_width=3,
             colour=color.dark4,
             padding_y=6,
-            padding_x=2,
+            group=True,
         ),
     ],
 }
@@ -29,26 +29,61 @@ widget = Widget(common_attrs=COMMON_ATTRS)
 
 # ----------------- EXTRA -----------------
 
+
+def draw_main_box(box: Box):
+    w = 20
+    h = 25
+    x = (box.size - w) // 2
+    y = (box.bar.height - h) // 2
+    box.drawer.ctx.rectangle(x, y, w, h)
+    box.drawer.set_source_rgb(color.cyan)
+    box.drawer.ctx.fill()
+
+def set_label_color(rule: GroupBoxRule, box: Box):
+    if box.focused:
+        rule.text_colour = color.dark1
+    elif box.occupied:
+        rule.text_colour = color.light1
+    else:
+        rule.text_colour = color.grey
+
+    return True
+
+
 groupbox_config = dict(
     fontsize=20,
     rounded=False,
-    highlight_method="block",
     this_current_screen_border=color.cyan,
+    padding_x=6,
+    padding_y=10,
+    rules=[
+        GroupBoxRule(custom_draw=draw_main_box).when(focused=True),
+        GroupBoxRule().when(func=set_label_color),
+    ],
 )
 
 
 # ---------------- ACTUAL BAR -----------------
 
 bar_left = [
-    widget.pad(),
+    widget.pad(length=6),
+    widget.pad(**COMMON_ATTRS),
     widget.groupbox(**groupbox_config),
+    widget.pad(**COMMON_ATTRS),
 ]
 
 bar_middle = [
     widget.spacer(),
 ]
 
-bar_right = []
+bar_right = [
+    widget.volume(fmt = " 󰕾 {} "),
+    widget.pad(length=8),
+    widget.clock(fmt=" {} "),
+    widget.pad(length=8),
+    widget.date(fmt=" {} "),
+    widget.pad(length=6),
+]
 
 minimal_bar = bar.Bar(
     bar_left + bar_middle + bar_right,
--- configs/qtile/config.py
+++ configs/qtile/config.py
@@ -1,13 +1,12 @@
 from themes.base import *
-from bars.groot_bar import groot_bar
+from bars.minimal import minimal_bar
 from libqtile import bar
 from libqtile.config import Screen
 from vars import wallpaper_path
 
-WORKSPACES = "󰎤 󰎧 󰎪 󰎭 󰎱".split()
-EXTRA_WORKSPACE = "󰅗"
-WALL1 = f"{wallpaper_path}/groot.png"
-WALL2 = f"{wallpaper_path}/blank.png"
+WORKSPACES = "1 2 3 4 5".split()
+EXTRA_WORKSPACE = "0"
+WALL = f"{wallpaper_path}/blank.png"
 
 groups.extend(
     configure_workspaces(WORKSPACES, EXTRA_WORKSPACE),
@@ -15,14 +14,14 @@ groups.extend(
 
 screens = [
     Screen(
-        top=groot_bar,
-        bottom=bar.Gap(6),
-        left=bar.Gap(6),
-        right=bar.Gap(6),
-        wallpaper=WALL1,
+        top=bar.Gap(1),
+        bottom=minimal_bar,
+        left=bar.Gap(1),
+        right=bar.Gap(1),
+        wallpaper=WALL,
     ),
     Screen(
-        wallpaper=WALL2,
+        wallpaper=WALL,
         wallpaper_mode="fill",
     ),
 ]
