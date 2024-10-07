63d5adfcbce8250f1fac609f1a635a60e2ed542c
kraanzu <kraanzu@gmail.com>
2024-10-07 20:57:19 +0530

    final fix: openrgb

--- modules/nixos/services/openrgb/default.nix
+++ modules/nixos/services/openrgb/default.nix
@@ -21,12 +21,14 @@ in {
   };
 
   config = mkIf cfg.enable {
-    services.hardware.openrgb.enable = true;
+    # services.hardware.openrgb.enable = true;
+    environment.systemPackages = [pkgs.openrgb];
+    services.udev.packages = [pkgs.openrgb];
 
     systemd.user.services.openrgb = {
       description = "OpenRGB Server";
       serviceConfig = {
-        ExecStart = "${pkgs.openrgb}/bin/openrgb --server --profile ${cfg.profile}";
+        ExecStart = "${pkgs.openrgb}/bin/openrgb --server --server-port 6742 --profile ${cfg.profile}";
         Restart = "on-failure";
       };
       wantedBy = ["default.target"];
