f3b2e72d009684795a51a0dc9b800b743a40ef9d
kraanzu <kraanzu@gmail.com>
2024-10-09 09:24:35 +0530

    fix: qtile start (move apply_theme to main config file)

--- configs/qtile/base_config.py
+++ configs/qtile/base_config.py
@@ -131,25 +131,3 @@ def start_once():
 # ------------------------------------------
 
 
-def apply_theme(theme: KzTheme):
-    global screens, groups
-
-    groups.extend(
-        configure_workspaces(
-            theme.workspaces,
-            theme.extra_workspace,
-        ),
-    )
-
-    wallpaper = f"{wallpaper_path}/{theme.wallpaper}.png"
-
-    screens = [
-        Screen(
-            top=bar.Gap(1),
-            bottom=theme.bar,
-            left=bar.Gap(1),
-            right=bar.Gap(1),
-            wallpaper=wallpaper,
-            wallpaper_mode="fill",
-        )
-    ]
--- configs/qtile/config.py
+++ configs/qtile/config.py
@@ -1,4 +1,27 @@
 from base_config import *
 
+def apply_theme(theme: KzTheme):
+    global screens, groups
+
+    groups.extend(
+        configure_workspaces(
+            theme.workspaces,
+            theme.extra_workspace,
+        ),
+    )
+
+    wallpaper = f"{wallpaper_path}/{theme.wallpaper}.png"
+
+    screens = [
+        Screen(
+            top=bar.Gap(1),
+            bottom=theme.bar,
+            left=bar.Gap(1),
+            right=bar.Gap(1),
+            wallpaper=wallpaper,
+            wallpaper_mode="fill",
+        )
+    ]
+
 theme = KzTheme.theme_wave()
 apply_theme(theme)
