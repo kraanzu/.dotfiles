2c1b5d72ad4dcc743d4c7bd22df739bcc9048a85
kraanzu <kraanzu@gmail.com>
2024-10-10 22:18:01 +0530

    better nix

--- modules/home-manager/mako/mako.nix
+++ modules/home-manager/mako/mako.nix
@@ -1,11 +1,23 @@
-let
+{
+  pkgs,
+  lib,
+  osConfig,
+  ...
+}: let
   bg = "#252a34";
   blue = "#81A1C1";
 in {
-  services.mako = {
-    enable = true;
-    # backgroundColor = bg;
-    # borderColor = blue;
-    defaultTimeout = 5000;
+  config = lib.mkIf osConfig.mynix.services.wayland.enable {
+    home.packages = with pkgs; [
+      mako
+      libnotify
+    ];
+    services.mako = {
+      enable = true;
+      backgroundColor = bg;
+      borderColor = blue;
+      borderSize = 3;
+      defaultTimeout = 5000;
+    };
   };
 }
