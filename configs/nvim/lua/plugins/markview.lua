ac968d8bfdf9cc7c55b5098a68eb405d51008baf
kraanzu <kraanzu@gmail.com>
2024-10-25 21:00:00 +0530

    disable markview

--- configs/nvim/lua/plugins/markview.lua
+++ configs/nvim/lua/plugins/markview.lua
@@ -1,10 +1,10 @@
 return {
-	"OXY2DEV/markview.nvim",
-	lazy = false, -- Recommended
-	-- ft = "markdown" -- If you decide to lazy-load anyway
-
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-		"nvim-tree/nvim-web-devicons",
-	},
+	-- "OXY2DEV/markview.nvim",
+	-- lazy = false, -- Recommended
+	-- -- ft = "markdown" -- If you decide to lazy-load anyway
+    --
+	-- dependencies = {
+	-- 	"nvim-treesitter/nvim-treesitter",
+	-- 	"nvim-tree/nvim-web-devicons",
+	-- },
 }
