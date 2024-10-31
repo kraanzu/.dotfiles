08314cb26b5920142418b8cfafff495102a8c436
kraanzu <kraanzu@gmail.com>
2024-10-31 11:31:28 +0530

    update flake.nix

--- configs/qtile/flake.nix
+++ configs/qtile/flake.nix
@@ -7,9 +7,9 @@
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
       in pkgs.mkShell {
         buildInputs = with pkgs; [
-          python311
-          python311Packages.qtile
-          python311Packages.qtile-extras
+          python3Packages.qtile
+          python3Packages.qtile-extras
+          python3Packages.requests
         ];
 
         shellHook = ''
