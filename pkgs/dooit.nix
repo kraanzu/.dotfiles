d7421038e4518dd26b6e6e7d672a4c78bb70ac1a
kraanzu <kraanzu@gmail.com>
2024-11-04 21:53:20 +0530

    update dooit to v3
    TODO: complete all todos

--- pkgs/dooit.nix
+++ pkgs/dooit.nix
@@ -10,47 +10,36 @@
 in
   python3.pkgs.buildPythonApplication rec {
     pname = "dooit";
-    version = "2.2.0";
+    version = "3.0.0";
     pyproject = true;
 
     src = fetchFromGitHub {
       owner = "kraanzu";
       repo = "dooit";
-      rev = "v${version}";
-      hash = "sha256-GtXRzj+o+FClleh73kqelk0JrSyafZhf847lX1BiS9k=";
+      rev = "develop"; # TODO: Change to version
+      hash = "sha256-N0r37iEj0P/qLBFG9bGPUPjw31Wk9kA+rrZd59Yrxd4=";
     };
 
     build-system = with python3.pkgs; [poetry-core];
 
     pythonRelaxDeps = [
       "tzlocal"
+      "textual"
     ];
 
     propagatedBuildInputs = with python3.pkgs; [
-      appdirs
       pyperclip
-      python-dateutil
+      textual
       pyyaml
-      (textual.overridePythonAttrs (oldAttrs: {
-        version = "0.47.1";
-        src = fetchFromGitHub {
-          owner = "Textualize";
-          repo = "textual";
-          rev = "refs/tags/v0.47.1";
-          hash = "sha256-RFaZKQ+0o6ZvfZxx95a1FjSHVJ0VOIAfzkdxYQXYBKU=";
-        };
-        disabledTests = [
-          "test_tracked_slugs"
-          "test_textual_env_var"
-          "test_register_language"
-          "test_register_language_existing_language"
-        ];
-      }))
+      dateutil
+      sqlalchemy
+      platformdirs
       tzlocal
+      click
     ];
 
     # No tests available
-    doCheck = false;
+    doCheck = true;
 
     passthru = {
       tests.version = testers.testVersion {
@@ -64,7 +53,7 @@ in
     meta = with lib; {
       description = "TUI todo manager";
       homepage = "https://github.com/kraanzu/dooit";
-      changelog = "https://github.com/kraanzu/dooit/blob/v${version}/CHANGELOG.md";
+      changelog = "https://github.com/kraanzu/dooit/blob/develop/CHANGELOG.md"; # TODO: change to version
       license = licenses.mit;
       maintainers = with maintainers; [
         khaneliman
