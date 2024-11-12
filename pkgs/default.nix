e40fa5cd0967b8773927fefdfe4e41ce5acbe189
kraanzu <kraanzu@gmail.com>
2024-11-12 15:14:41 +0530

    move projexts to packages

--- pkgs/default.nix
+++
@@ -1,6 +0,0 @@
-# Custom packages, that can be defined similarly to ones from nixpkgs
-# You can build them using 'nix build .#example'
-pkgs: {
-  # dooit = pkgs.callPackage ./dooit.nix {};
-  smassh = pkgs.callPackage ./smassh.nix {};
-}
