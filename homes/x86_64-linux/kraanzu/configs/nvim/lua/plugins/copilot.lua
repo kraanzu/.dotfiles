c5adcb956e6ae75731e0ed9b9597d2212a612bc5
kraanzu <kraanzu@gmail.com>
2024-12-08 20:08:35 +0530

    disable copilot

--- homes/x86_64-linux/kraanzu/configs/nvim/lua/plugins/copilot.lua
+++ homes/x86_64-linux/kraanzu/configs/nvim/lua/plugins/copilot.lua
@@ -1,23 +1,23 @@
 return {
-	"zbirenbaum/copilot.lua",
-	cmd = "Copilot",
-	event = "InsertEnter",
-	config = function()
-		require("copilot").setup({
-			suggestion = {
-				enabled = true,
-				auto_trigger = true,
-				hide_during_completion = false,
-				debounce = 75,
-				keymap = {
-					accept = "<M-l>",
-					accept_word = false,
-					accept_line = false,
-					next = "<M-]>",
-					prev = "<M-[>",
-					dismiss = "<C-]>",
-				},
-			},
-		})
-	end,
+	-- "zbirenbaum/copilot.lua",
+	-- cmd = "Copilot",
+	-- event = "InsertEnter",
+	-- config = function()
+	-- 	require("copilot").setup({
+	-- 		suggestion = {
+	-- 			enabled = true,
+	-- 			auto_trigger = true,
+	-- 			hide_during_completion = false,
+	-- 			debounce = 75,
+	-- 			keymap = {
+	-- 				accept = "<M-l>",
+	-- 				accept_word = false,
+	-- 				accept_line = false,
+	-- 				next = "<M-]>",
+	-- 				prev = "<M-[>",
+	-- 				dismiss = "<C-]>",
+	-- 			},
+	-- 		},
+	-- 	})
+	-- end,
 }
