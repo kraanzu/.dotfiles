ff679173b44e572a93fe2c753e2dc011aaeea915
kraanzu <kraanzu@gmail.com>
2024-12-08 20:09:15 +0530

    add tcss lua

--- homes/x86_64-linux/kraanzu/configs/nvim/after/plugin/tcss.lua
+++ homes/x86_64-linux/kraanzu/configs/nvim/after/plugin/tcss.lua
@@ -1,8 +1,23 @@
+vim.api.nvim_command("autocmd BufNewFile,BufRead *.tcss set filetype=tcss")
+
 local client = vim.lsp.start_client({
-	name = "tcss-lsp",
+	name = "tcss-lsp-v1",
 	cmd = { "/home/kraanzu/Projects/tcss-lsp/tcss-lsp" },
+	filetypes = { "tcss" },
+	root_dir = vim.fn.getcwd(),
+	autostart = true,
 })
 
-if not client then
+if client then
+	print("tcss-lsp started successfully")
+else
 	print("Failed to start tcss-lsp")
 end
+
+-- Manually call the function to start the LSP
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "tcss",
+	callback = function()
+		vim.lsp.buf_attach_client(0, client)
+	end,
+})
