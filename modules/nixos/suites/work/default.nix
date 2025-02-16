4eb66075ff5342f23b9778693b41ad1947d9cfcd
kraanzu <kraanzu@gmail.com>
2025-02-16 10:51:45 +0530

    add work suite

---
+++ modules/nixos/suites/work/default.nix
@@ -0,0 +1,25 @@
+{
+  namespace,
+  lib,
+  config,
+  ...
+}:
+with lib;
+with lib.${namespace}; let
+  cfg = config.${namespace}.suites.work;
+in {
+  options.${namespace}.suites.work.enable = lib.mkOption {
+    type = lib.types.bool;
+    default = false;
+    description = "Work Suite";
+  };
+
+  config = mkIf cfg.enable {
+    mynix = {
+      apps = {
+        slack = enable;
+        chrome = enable;
+      };
+    };
+  };
+}
