5298943b3e3374319646fa1775c3b40e617fc302
kraanzu <kraanzu@gmail.com>
2025-02-22 07:11:17 -0500

    setup media suite

---
+++ modules/darwin/suites/media/default.nix
@@ -0,0 +1,29 @@
+{
+  namespace,
+  lib,
+  config,
+  ...
+}:
+with lib;
+with lib.${namespace}; let
+  cfg = config.${namespace}.suites.media;
+in {
+  options.${namespace}.suites.media.enable = lib.mkOption {
+    type = lib.types.bool;
+    default = false;
+    description = "Media Suite";
+  };
+
+  config = mkIf cfg.enable {
+    mynix = {
+      apps = {
+        vlc = enabled;
+        anydesk = enabled;
+      };
+
+      tools = {
+        gdrive = enabled;
+      };
+    };
+  };
+}
