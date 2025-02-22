2c4c190097aec911b138289f1cb8911554c19a23
kraanzu <kraanzu@gmail.com>
2025-02-22 07:13:44 -0500

    setup social suite

---
+++ modules/darwin/suites/social/default.nix
@@ -0,0 +1,25 @@
+{
+  namespace,
+  lib,
+  config,
+  ...
+}:
+with lib;
+with lib.${namespace}; let
+  cfg = config.${namespace}.suites.social;
+in {
+  options.${namespace}.suites.social.enable = lib.mkOption {
+    type = lib.types.bool;
+    default = false;
+    description = "Social Suite";
+  };
+
+  config = mkIf cfg.enable {
+    mynix = {
+      apps = {
+        discord = enabled;
+        slack = enabled;
+      };
+    };
+  };
+}
