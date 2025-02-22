8651cb0fad88bae66ff679ad27df5de633b4bfc7
kraanzu <kraanzu@gmail.com>
2025-02-22 07:09:01 -0500

    setup dev suite

---
+++ modules/darwin/suites/development/default.nix
@@ -0,0 +1,34 @@
+{
+  namespace,
+  lib,
+  config,
+  ...
+}:
+with lib;
+with lib.${namespace}; let
+  cfg = config.${namespace}.suites.development;
+in {
+  options.${namespace}.suites.development.enable = lib.mkOption {
+    type = lib.types.bool;
+    default = false;
+    description = "Development Config";
+  };
+
+  config = mkIf cfg.enable {
+    ${namespace} = {
+      dev.lang = {
+        python = enabled;
+        nix = enabled;
+        lua = enabled;
+        markdown = enabled;
+      };
+      tools = {
+        wezterm = enabled;
+        docker = enabled;
+        vscode = enabled;
+        code-cursor = enabled;
+        zed = enabled;
+      };
+    };
+  };
+}
