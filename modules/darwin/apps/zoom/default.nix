9a5526300b4f1d4256abc1e85a1dd539fa3df3e8
kraanzu <kraanzu@gmail.com>
2025-02-25 07:59:29 -0500

    fix: zoom override

--- modules/darwin/apps/zoom/default.nix
+++ modules/darwin/apps/zoom/default.nix
@@ -8,17 +8,5 @@
 with lib; let
   cfg = config.${namespace}.apps.zoom;
 in {
-  options.${namespace}.apps.zoom = {
-    enable = mkOption {
-      type = types.bool;
-      default = false;
-      description = "Enable zoom";
-    };
-  };
-
-  config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      zoom-us
-    ];
-  };
+  imports = [ (lib.snowfall.fs.get-file "modules/shared/apps/zoom/default.nix") ];
 }
