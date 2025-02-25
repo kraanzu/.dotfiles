5a76ac7987ff0b6c5315ffd77dcbff66396fd6b7
kraanzu <kraanzu@gmail.com>
2025-02-25 07:33:08 -0500

    add fonts shared module

---
+++ modules/shared/system/fonts/default.nix
@@ -0,0 +1,19 @@
+{
+  pkgs,
+  namespace,
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.${namespace}.system.fonts;
+in {
+  options.${namespace}.system.fonts.enable = lib.mkOption {
+    type = lib.types.bool;
+    default = false;
+    description = "Setup fonts";
+  };
+
+  config = lib.mkIf cfg.enable {
+    fonts.packages = with pkgs; [nerd-fonts.sauce-code-pro nerd-fonts.jetbrains-mono];
+  };
+}
