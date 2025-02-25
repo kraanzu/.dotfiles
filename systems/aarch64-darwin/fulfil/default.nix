c9715813224df8e1b467076832e2ecac50bb935b
kraanzu <kraanzu@gmail.com>
2025-02-25 03:03:43 -0500

    spotlight symlink fix

--- systems/aarch64-darwin/fulfil/default.nix
+++ systems/aarch64-darwin/fulfil/default.nix
@@ -2,6 +2,7 @@
   lib,
   pkgs,
   namespace,
+  config,
   ...
 }:
 with lib.${namespace};
@@ -10,6 +11,27 @@ with lib.${namespace};
   ids.gids.nixbld = 350;
   system.stateVersion = 4;
   homebrew.enable = true;
+
+  system.activationScripts.applications.text =
+    let
+      env = pkgs.buildEnv {
+        name = "system-applications";
+        paths = config.environment.systemPackages;
+        pathsToLink = "/Applications";
+      };
+    in
+    pkgs.lib.mkForce ''
+      # Set up applications.
+      echo "setting up /Applications..." >&2
+      rm -rf /Applications/Nix\ Apps
+      mkdir -p /Applications/Nix\ Apps
+      find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+      while read -r src; do
+        app_name=$(basename "$src")
+        echo "copying $src" >&2
+        ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+      done
+    '';
 
   ${namespace} = {
     archetypes = {
