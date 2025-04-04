4c841ad40d235ad61d81985d3dd7821031754ab9
kraanzu <kraanzu@gmail.com>
2025-04-04 20:34:54 +0530

    Replace deprecated

--- modules/home/apps/neovim/config/lua/plugins/line.lua
+++ modules/home/apps/neovim/config/lua/plugins/line.lua
@@ -1,7 +1,7 @@
 local function show_lsp()
 	local msg = "No Active Lsp"
 	local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-	local clients = vim.lsp.get_active_clients()
+	local clients = vim.lsp.get_clients()
 	if next(clients) == nil then
 		return msg
 	end
