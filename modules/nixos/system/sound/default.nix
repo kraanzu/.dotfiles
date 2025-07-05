5d6552b0c7eab3abfd7a23f38dee3e6cc77f5459
kraanzu <kraanzu@gmail.com>
2025-07-05 07:43:32 +0530

    Remember devices for bt

--- modules/nixos/system/sound/default.nix
+++ modules/nixos/system/sound/default.nix
@@ -16,22 +16,25 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    # Enable sound with pipewire.
     services.pulseaudio.enable = false;
-    hardware.bluetooth.enable = true; # enables support for Bluetooth
-    hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+
+    hardware.bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+      settings = {
+        General = {
+          AutoEnable = true;
+          RememberDevice = true;
+        };
+      };
+    };
+
     security.rtkit.enable = true;
     services.pipewire = {
       enable = true;
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
-      # If you want to use JACK applications, uncomment this
-      #jack.enable = true;
-
-      # use the example session manager (no others are packaged yet so this is enabled by default,
-      # no need to redefine it in your config for now)
-      #media-session.enable = true;
     };
   };
 }
