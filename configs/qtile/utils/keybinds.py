ddc8ad52eaf277f95f7f7dcd4d120e605ae58c62
kraanzu <kraanzu@gmail.com>
2025-09-21 16:11:36 +0530

    Keybind for brave

--- configs/qtile/utils/keybinds.py
+++ configs/qtile/utils/keybinds.py
@@ -42,6 +42,11 @@ key_bindings = [
         desc="Launches Terminal",
     ),
     Keybind(
+        "M-b",
+        lazy.spawn("brave"),
+        desc="Launches Terminal",
+    ),
+    Keybind(
         "M-s",
         lazy.group["scratchpad"].dropdown_toggle("term"),
         desc="Launches Terminal Scratchpad",
