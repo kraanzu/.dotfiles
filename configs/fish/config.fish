a850ae430cfdf04afcebcf4b742047709938b7fa
kraanzu <kraanzu@gmail.com>
2025-10-01 10:39:59 +0530

    Fishy fish changes

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -2,22 +2,19 @@
 source $HOME/.config/fish/secrets.fish 2> /dev/null
 source $HOME/.config/fish/utils.fish
 source $HOME/.config/fish/quick_scripts.fish
-
-#--------CUSTOM FUNCTIONS---------------
-fish_vi_key_bindings
+source $HOME/.conda/etc/fish/conf.d/conda.fish 2> /dev/null
 
 #----------------------------- BASICS -----------------------------------
-set fish_greeting # Supresses fish's intro message
-set TERM xterm-256color # Sets the terminal type
-
-set EDITOR nvim #Sets Editor to neovim
-set VISUAL nvim
-
+fish_vi_key_bindings
 fish_config theme choose Nord
+
+set fish_greeting # Supresses fish's intro message
+set TERM xterm-256color
+set EDITOR nvim
+set VISUAL nvim
 
 #----------------------------- ALIASES --------------------------------
 
-#----DIRECTORY CHANGE
 alias ..='cd ..'
 alias ...='cd ../..'
 
@@ -25,31 +22,24 @@ alias vim='nvim'
 alias tmux='tmux -u'
 alias mv='mv -i'
 alias rm='rm -rfi'
+alias cp='cp -r'
 
-#----LS OUTPUT
 alias ls='exa -F --color=always --group-directories-first' # my preferred listing
 alias la='exa -a --color=always --group-directories-first' # all files and dirs
 alias ll='exa -l --color=always --group-directories-first' # long format
 alias lt='exa -aT --color=always --group-directories-first' # tree listing
-
-# adding flags
 alias df='df -h | grep -e "^/dev"' # human-readable sizes
 alias free='free -m' # show sizes in MB
 alias yts='yt-dlp --extract-audio --audio-format opus' # download youtube video as mp3
-
-
-# GREP COLOR
+alias cat='bat -pp --theme=Nord'
 alias grep='grep --color=auto'
-
-export PYTHONDONTWRITEBYTECODE=1
-export GOPATH=$HOME/.local/go
-export IPYTHONDIR=~/.config/ipython
-
-set NPM_PACKAGES "$HOME/.npm-packages"
-set PATH $PATH $NPM_PACKAGES/bin
+alias cb='xclip -selection clipboard'
 
 export LANG=en_IN.UTF-8
 export TERM=xterm-256color
+export PYTHONDONTWRITEBYTECODE=1
+export GOPATH=$HOME/.local/go
+export IPYTHONDIR=~/.config/ipython
 
 # Extra programs
 syncthing install-completions | source 2> /dev/null
