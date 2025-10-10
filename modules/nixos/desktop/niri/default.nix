ea1b070dfa5731547a6a87828329e6233d0eeca7
kraanzu <kraanzu@gmail.com>
2025-10-10 16:20:53 +0530

    Add niri + waybar :)

---
+++ modules/nixos/desktop/niri/default.nix
@@ -0,0 +1,22 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.mynix.desktop.niri;
+in
+{
+  options = {
+    mynix.desktop.niri.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable niri WM";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.niri.enable = true;
+    programs.waybar.enable = true;
+  };
+}
--- systems/x86_64-linux/nzxt/default.nix
+++ systems/x86_64-linux/nzxt/default.nix
@@ -24,6 +24,7 @@ with lib.mynix;
     };
     desktop = {
       qtile = enabled;
+      niri = enabled;
       login = enabled;
       utilities = enabled;
     };
