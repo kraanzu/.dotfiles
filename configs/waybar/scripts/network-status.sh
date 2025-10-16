5529d1720830fe4bf5de7ffe08a5036e6b543b94
kraanzu <kraanzu@gmail.com>
2025-10-16 20:57:20 +0530

    Update network code

--- configs/waybar/scripts/network-status.sh
+++ configs/waybar/scripts/network-status.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
-
 device=$(nmcli -g DEVICE connection show --active | grep -E "^enp" | head -n1)
 
-case $device in
-    "enp7s0")
-        echo "LEAF"
-        ;;
-    "enp12s0f3u3")
-        echo "S23"
-        ;;
-    *)
-        echo "$device"
-        ;;
-esac
+if [ -n "$device" ]; then
+    vendor=$(nmcli -t -f GENERAL.VENDOR device show "$device" 2>/dev/null | cut -d: -f2 | awk '{print $1}')
+    echo "${vendor:-$device}"
+else
+    echo "No device"
+fi
