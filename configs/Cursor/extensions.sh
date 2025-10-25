279d6fc9730da4e02a969425459c4c11af069126
kraanzu <kraanzu@gmail.com>
2025-10-25 13:42:51 +0530

    Cursor settings

---
+++ configs/Cursor/extensions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Read desired extensions from file
+mapfile -t desired_extensions < extensions.txt
+
+# Get currently installed extensions
+mapfile -t installed_extensions < <(cursor --list-extensions)
+
+# Install desired extensions
+for ext in "${desired_extensions[@]}"; do
+    cursor --install-extension "$ext"
+done
+
+# Uninstall extensions not in the desired list
+for ext in "${installed_extensions[@]}"; do
+    if [[ ! " ${desired_extensions[@]} " =~ " ${ext} " ]]; then
+        cursor --uninstall-extension "$ext"
+    fi
+done
