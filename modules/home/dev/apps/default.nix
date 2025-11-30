57fdf0990890f1d8dc5658e12d723cba3834222b
kraanzu <kraanzu@gmail.com>
2025-11-30 16:09:43 +0530

    Remove vscode

--- modules/home/dev/apps/default.nix
+++ modules/home/dev/apps/default.nix
@@ -16,7 +16,6 @@ in
     wezterm.enable = mkBoolOpt false "Enable WezTerm";
     neovim.enable = mkBoolOpt false "Enable Neovim";
     postman.enable = mkBoolOpt false "Enable Postman";
-    vscode.enable = mkBoolOpt false "Enable VSCode";
     cursor.enable = mkBoolOpt false "Enable Cursor";
     zed.enable = mkBoolOpt false "Enable Zed";
   };
@@ -25,7 +24,6 @@ in
       home.packages = with pkgs; [
         (mkIf (cfg.alacritty.enable || cfg.all.enable) alacritty)
         (mkIf (cfg.postman.enable || cfg.all.enable) postman)
-        (mkIf (cfg.vscode.enable || cfg.all.enable) vscode)
         (mkIf (cfg.cursor.enable || cfg.all.enable) code-cursor)
         (mkIf (cfg.wezterm.enable || cfg.all.enable) wezterm)
         (mkIf (cfg.zed.enable || cfg.all.enable) zed-editor)
