a570eb1735d21a90f0ddcc13b4050a90c1f1c873
kraanzu <kraanzu@gmail.com>
2026-02-21 09:26:26 +0530

    Dont read the opvn file

--- modules/nixos/cli/openvpn/default.nix
+++ modules/nixos/cli/openvpn/default.nix
@@ -15,29 +15,25 @@ in
       description = "Enable OpenVPN";
     };
   };
-
   config = lib.mkIf cfg.enable {
     boot.kernelModules = [ "tun" ];
     environment.etc.openvpn.source = "${pkgs.update-resolv-conf}/libexec/openvpn";
-
     services.openvpn.servers = {
       sp_htb = {
-        config = builtins.readFile ./sp_htb.ovpn;
+        config = "config ${./sp_htb.ovpn}";
         autoStart = false;
       };
       lab_htb = {
-        config = builtins.readFile ./lab_htb.ovpn;
+        config = "config ${./lab_htb.ovpn}";
         autoStart = false;
       };
       proton = {
-        config = builtins.readFile ./proton.ovpn;
+        config = "config ${./proton.ovpn}";
         authUserPass = builtins.toString ./proton.secret;
         autoStart = false;
         updateResolvConf = false;
       };
     };
-
     environment.systemPackages = [ pkgs.openvpn ];
-
   };
 }
