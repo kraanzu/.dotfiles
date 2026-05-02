55a56e6e127c5ddf95efd7d52b473357e9e5828a
kraanzu <kraanzu@gmail.com>
2026-05-02 20:47:55 +0530

    Add virt manager

---
+++ modules/nixos/cli/virt/default.nix
@@ -0,0 +1,39 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.mynix.cli.virt;
+in
+{
+  options = {
+    mynix.cli.virt.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable Virt manager";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    virtualisation.libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        runAsRoot = true;
+        swtpm.enable = true;
+      };
+    };
+    environment.systemPackages = with pkgs; [
+      virt-manager
+      virtiofsd
+      qemu_kvm
+    ];
+
+    users.users.kraanzu.extraGroups = [
+      "libvirtd"
+      "kvm"
+    ];
+  };
+}
