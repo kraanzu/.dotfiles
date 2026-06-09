593b242f73978569aa874a878aca6e591f0f8b50
kraanzu <kraanzu@gmail.com>
2026-06-09 20:36:52 +0530

    Remove bitwarden desktop

--- modules/home/privacy/default.nix
+++ modules/home/privacy/default.nix
@@ -14,14 +14,11 @@ in
 {
   options.mynix.secrets = {
     all.enable = mkBoolOpt true "Enable all privacy apps";
-
-    bitwarden.enable = mkBoolOpt false "Enable bitwarden";
     gitcrypt.enable = mkBoolOpt false "Enable git-crypt";
   };
 
   config = {
     home.packages = with pkgs; [
-      (mkIf (cfg.bitwarden.enable || cfg.all.enable) bitwarden-desktop)
       (mkIf (cfg.gitcrypt.enable || cfg.all.enable) git-crypt)
     ];
   };
